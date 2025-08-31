f77e0244ffd8f06e7f44bf08088145e163ca60fa
Pattern Recognition and Bioimage Informatics Group <84379335+PRBioimages@users.noreply.github.com>
2025-09-01 07:44:09 +0800

    Delete reconstruction directory

--- reconstruction/ResAE/dataloaders/__init__.py
+++
@@ -1,6 +0,0 @@
-from reconstruction.ResAE.dataloaders.experiments import *
-from reconstruction.ResAE.configs import Config
-
-
-def get_dataloader(cfg: Config):
-    return globals().get(cfg.experiment.name)
